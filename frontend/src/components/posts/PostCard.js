8499d3f8a3ace543ac2599de8be14987a2a9145c
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-18 15:57:33 +0100

    created postform and postpage

---
+++ frontend/src/components/posts/PostCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PostCard() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">SDA starter template</h4>
+                <p>This starter template is based on Spring, PostgreSQL, React, React router and Axios. Check the following 
+                    links for documentation and guides:</p>
+            </div>
+        </div>
+    );
+}
+
+export default PostCard;
--- frontend/src/components/posts/PostsPage.js
+++ frontend/src/components/posts/PostsPage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import PostCard from "./PostCard";
+import PostsApi from '../../api/PostsApi';
+import PostForm from './PostForm';
 
 function PostsPage() {
+    //console.log(PostsApi.getAllPosts());
     return (
         <div>
+             Posts page
+             <PostForm />
+
         </div>
+       
     );
 }
 
