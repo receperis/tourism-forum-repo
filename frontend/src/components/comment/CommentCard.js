6db92bd8907eef7f480788df7f9ea9dc3a18830f
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-19 22:01:27 +0100

    changed functionality

--- frontend/src/components/comment/CommentCard.js
+++ frontend/src/components/comment/CommentCard.js
@@ -4,7 +4,10 @@ import Comment from './Comment';
 function CommentCard({comments, onCommentDelete}) {
     return <div className="mt-4">
         {
-        comments.map(comment => <Comment key={comment.id} comment= {comment} onCommentDelete = {onCommentDelete} />)
+        comments.map(comment => <Comment 
+                key={comment.id} 
+                comment= {comment} 
+                onCommentDelete = {onCommentDelete} />)
         }
     </div>
 }
