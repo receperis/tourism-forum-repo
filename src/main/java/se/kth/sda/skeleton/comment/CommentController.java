b9d662aecdf65c318f0287a6e18d788fbb232b93
Elena Kolpakova <kolpakova.elena.p@gmail.com>
2020-11-19 12:44:10 +0100

    refactor function with extraction of the user

--- src/main/java/se/kth/sda/skeleton/comment/CommentController.java
+++ src/main/java/se/kth/sda/skeleton/comment/CommentController.java
@@ -4,8 +4,13 @@ package se.kth.sda.skeleton.comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import se.kth.sda.skeleton.user.User;
+import se.kth.sda.skeleton.user.UserService;
 
 
 import java.util.List;
@@ -16,6 +21,9 @@ public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("")
     public List<Comment> getAll(@RequestParam(required = false) Long postId){
@@ -37,16 +45,26 @@ public class CommentController {
 
     @PostMapping("")
     public Comment create(@RequestBody Comment newComment){
+        newComment.setUser(extractUserFromAuth());
         return commentService.create(newComment);
     }
 
     @PutMapping("")
     public Comment update(@RequestBody Comment updatedComment){
+        updatedComment.setUser(extractUserFromAuth());
         return commentService.update(updatedComment);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         commentService.delete(id);
+    }
+
+    //extracting information about the user from current session (not from front end)
+    public User extractUserFromAuth()
+    {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        return  userService.findUserByEmail(userDetails.getUsername());
     }
 }
