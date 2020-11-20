6bab69ab871ebf9c75b78e99a4574ac6585560e2
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-20 12:35:39 +0100

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,49 @@
-import React, {useState} from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Import custom styles for our application
-import './App.css';
+import "./App.css";
 
-import Auth from './services/Auth';
+import Auth from "./services/Auth";
 import Navbar from "./components/layout/Navbar";
 
 // Import pages
 import LoginPage from "./components/auth/LoginPage";
-import HomePage from './components/home/HomePage';
+import HomePage from "./components/home/HomePage";
 import PostsPage from "./components/posts/PostsPage";
-import ChatPage from './components/chat/ChatPage';
+import CommentsPage from "./components/comments/CommentsPage";
+import CommentPageDetails from "./components/comments/CommentPageDetails";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(Auth.isLoggedIn());
   Auth.bindLoggedInStateSetter(setLoggedIn);
-  
+
   const loggedInRouter = (
-            <Router>
-                <Navbar onLogout={() => Auth.logout()} />
+    <Router>
+      <Navbar onLogout={() => Auth.logout()} />
 
-                <div className="container mt-5">
-                    <Switch>
-                        <Route path="/posts">
-                            <PostsPage/>
-                        </Route>
+      <div className="container mt-5">
+        <Switch>
+          <Route path="/posts">
+            <PostsPage />
+          </Route>
+          <Route path="/comments">
+            <CommentsPage />
+          </Route>
 
-                        <Route path="/chat">
-                            <ChatPage/>
-                        </Route>
+          <Route path="/comments/:id">
+            <CommentPageDetails />
+          </Route>
 
-                        <Route path="/">
-                          <HomePage/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 
-  return (loggedIn ? loggedInRouter : <LoginPage/>);
+  return loggedIn ? loggedInRouter : <LoginPage />;
 }
 
 export default App;
