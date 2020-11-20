cd8fb25e0dac9a5352fa36ec7120293117ebe011
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-20 12:36:25 +0100

    added functionality to comment

--- frontend/src/components/posts/PostsList.js
+++ frontend/src/components/posts/PostsList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Post from './Post';
 
-function PostsList({posts, onPostUpdate, onPostDelete}) {
+function PostsList({posts, onPostUpdate, onPostDelete, onPostComment}) {
     return <div className="mt-4">
         {
           posts.map(post => (<Post 
                 key={post.id}
                 post={post}
                 onPostUpdate = {onPostUpdate}
-                onPostDelete={onPostDelete}/>)
+                onPostDelete={onPostDelete}
+                onPostComment= {onPostComment}/>)
          )  
         }
     </div>
