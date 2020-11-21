0eb0f9a2872d9c5cc589b74b6c869c2685237e56
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-21 16:20:35 +0100

    added comments list from posts page to comments page

--- frontend/src/components/comments/CommentsList.js
+++ frontend/src/components/comments/CommentsList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Comment from './Comment';
 
 function CommentsList({comment, onCommentDelete, onCommentUpdate}) {
-    console.log("comments details",comment);
     return <div className="mt-4">
         {
         comment.map(comment => <Comment 
