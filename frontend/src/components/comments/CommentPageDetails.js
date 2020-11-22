5a893ff79b2f3e97173a2b1d5a3ac404d6b55050
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-22 17:47:05 +0100

    added comment details page

--- frontend/src/components/comments/CommentPageDetails.js
+++ frontend/src/components/comments/CommentPageDetails.js
@@ -1,8 +1,36 @@
-import React from 'react';
-export default function CommentPageDetails() {
+import React ,{ useState, useEffect }from 'react';
+import CommentsList from './CommentsList';
+import CommentsApi from "../../api/CommentsApi";
+
+export default function CommentPageDetails({match}) {
+    console.log("match",match.params.id);
+
+    const id = Number(match.params.id);
+    console.log("id",id);
+    const [comments, setComments] = useState([]);
+
+    const getAllCommentsByPostId = () => {
+        return CommentsApi.getCommentById(match.params.id) 
+            .then(res => setComments(res.data));
+           // .then(res => console.log("comments data",res.data));
+    }
+   
+    useEffect(() => {
+        getAllCommentsByPostId();
+    });
     return (
-        <div>
-            CommentPageDetails
-        </div>
-    )
+        
+            <div>
+            
+               CommentPageDetails
+               {
+                   <CommentsList
+                        comment= {comments}
+                     /*    onCommentDelete = {deleteComment} 
+                        onCommentUpdate = {updateComment} */
+                    />
+               }
+            </div>
+        );
+  
 }
