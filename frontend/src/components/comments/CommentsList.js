8dd0967531c810be96d841ebdf215d030aec83af
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-22 17:47:30 +0100

    added comment details to comments page

--- frontend/src/components/comments/CommentsList.js
+++ frontend/src/components/comments/CommentsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Comment from './Comment';
 
 function CommentsList({comment, onCommentDelete, onCommentUpdate}) {
+    console.log("comments details",comment);
     return <div className="mt-4">
         {
         comment.map(comment => <Comment 
