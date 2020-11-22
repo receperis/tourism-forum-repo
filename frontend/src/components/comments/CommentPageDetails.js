a81699d98aae13a12701185d67fd79f91640caae
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-22 21:35:39 +0100

    added functionality for deleting the comment

--- frontend/src/components/comments/CommentPageDetails.js
+++ frontend/src/components/comments/CommentPageDetails.js
@@ -2,11 +2,8 @@ import React ,{ useState, useEffect }from 'react';
 import CommentsList from './CommentsList';
 import CommentsApi from "../../api/CommentsApi";
 
-export default function CommentPageDetails({match}) {
-    console.log("match",match.params.id);
 
-    const id = Number(match.params.id);
-    console.log("id",id);
+export default function CommentPageDetails({match}) {
     const [comments, setComments] = useState([]);
 
     const getAllCommentsByPostId = () => {
@@ -18,14 +15,18 @@ export default function CommentPageDetails({match}) {
         getAllCommentsByPostId();
     }, []);
 
+    const deleteComment = (comment) => {
+        return CommentsApi.deleteComment(comment.id)
+             .then(() => setComments(comments.filter(a => a.id !== comment.id)));
+    }
+
     return (
             <div>
               <h1>All the comments to post id = {match.params.id} </h1>
                {
                    <CommentsList
                         comment= {comments}
-                     /*    onCommentDelete = {deleteComment} 
-                        onCommentUpdate = {updateComment} */
+                        onCommentDelete = {deleteComment} 
                     />
                }
             </div>
